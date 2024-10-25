62a78b3231123c98644aae28392b6dd2ab13c3b9
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-10-25 11:05:33 +0530

    added student and video model

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,8 +1,74 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const studentSchema = new mongoose.Schema({
-    
+    username:{
+        type: String,
+        required:true,
+        index:true
+    },
+    email:{
+        type: String,
+        required:true,
+        unique: true,
+        lowercase:true,
+        trim:true
+    },
+    avatar:{
+        type: String, //cloudinary url
+        required:true,
+    },
+    password:{
+        type:String,
+        required: [true, "Password is required"]
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    refreshToken:{
+        type:String
+    }
 }, {timestamps: true})
 
+studentSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next()
 
-export const Student = mongoose.model("User", studentSchema)
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+studentSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+ }
+
+studentSchema.methods.generateAccessToken =  function () {
+    return jwt.sign(
+     {
+         _id : this._id,
+         email : this.email,
+         username : this.username    
+     },
+     process.env.ACCESS_TOKEN_SECRET,
+     {
+         expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+     }
+    )
+ }
+
+studentSchema.methods.generateRefreshToken =  function () {
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+       )
+}
+
+
+
+export const Student = mongoose.model("Student", studentSchema)
